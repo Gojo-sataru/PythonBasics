8e9381c72ad82af165afef151bc72cc632c468f3
Joshua <joshua.emptyslate@gmail.com>
2024-10-07 18:35:16 +0530

    functions upadte

---
+++ PythonBasics/Functions.py
@@ -0,0 +1,31 @@
+def add():
+    print("addition")
+    a=int(input("enter a nymber="))
+    b=int(input("enter a nymber="))
+    print(a+b)
+
+add()
+
+def sub():
+    print("subration")
+    a=int(input("enter a nymber="))
+    b=int(input("enter a nymber="))
+    print(a-b)
+
+sub()
+
+def div():
+    print("divition")
+    a=int(input("enter a nymber="))
+    b=int(input("enter a nymber="))
+    print(a/b)
+
+div()
+
+def mul():
+    print("multiply")
+    a=int(input("enter a nymber="))
+    b=int(input("enter a nymber="))
+    print(a*b)
+
+mul()
