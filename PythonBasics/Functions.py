47d86f80d408ebd7f498a95758840455461a70d6
Joshua <joshua.emptyslate@gmail.com>
2024-10-09 15:41:04 +0530

    functions examples

--- PythonBasics/Functions.py
+++ PythonBasics/Functions.py
@@ -1,31 +1,99 @@
-def add():
-    print("addition")
-    a=int(input("enter a nymber="))
-    b=int(input("enter a nymber="))
-    print(a+b)
+# def add():
+#     print("addition")
+#     a=int(input("enter a nymber="))
+#     b=int(input("enter a nymber="))
+#     print(a+b)
+#
+# add()
+#
+# def sub():
+#     print("subration")
+#     a=int(input("enter a nymber="))
+#     b=int(input("enter a nymber="))
+#     print(a-b)
+#
+# sub()
+#
+# def div():
+#     print("divition")
+#     a=int(input("enter a nymber="))
+#     b=int(input("enter a nymber="))
+#     print(a/b)
+#
+# div()
+#
+# def mul():
+#     print("multiply")
+#     a=int(input("enter a nymber="))
+#     b=int(input("enter a nymber="))
+#     print(a*b)
+#
+# mul()
 
-add()
 
-def sub():
-    print("subration")
-    a=int(input("enter a nymber="))
-    b=int(input("enter a nymber="))
-    print(a-b)
+#ex
 
-sub()
+# def greet_user(name):
+#     print(f"Hello, {name}!")
+# greet_user("Alice")
 
-def div():
-    print("divition")
-    a=int(input("enter a nymber="))
-    b=int(input("enter a nymber="))
-    print(a/b)
+# def add(a , b):
+#     return a+b
+#
+# print (add)
+# add(7,7)
+#
 
-div()
+#Function example with arguments
+# def new_len(one,two):
+#     print(one,two)
+#
+# new_len("start","Succed")
 
-def mul():
-    print("multiply")
-    a=int(input("enter a nymber="))
-    b=int(input("enter a nymber="))
-    print(a*b)
+# def old(*king):
+#     print("the new one is",king[1])
+#
+# old("kinglius","king phhilphines",)
 
-mul()
+# EXAMPLE one for passing list as arguments
+
+# def function(foods):
+#     for x in foods:
+#         print(x)
+#
+# fruits={"apple",1,2,3,"cherry"}
+#
+# function(fruits)
+
+# lets see similar examples with this
+
+def my_function():
+    fruits = ["apple",1,2,3, "cherry"]
+    for x in fruits:
+        print(x)
+
+my_function()
+
+# #function with return values
+# def function(x):
+#     return 5 * x
+#
+# print(function(5))
+# print (function(7))
+# print (function(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
